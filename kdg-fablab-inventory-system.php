952eff1c02fad29e9dfccb1840c3c1be0d38cc01
kevin-st <kevin.stollaerd@gmail.com>
2019-04-10 16:48:19 +0200

    Plugin setup

---
+++ kdg-fablab-inventory-system.php
@@ -0,0 +1,41 @@
+<?php
+  /**
+   * @package KdG_Fablab_Inventory_System
+   */
+  /*
+    Plugin Name: KdG Fablab Inventory System
+    Description: Voeg machines toe, bewerk ze en verwijder ze in het KdG Fablab.
+    Author: K3
+    Version: 1.0.0
+    Liscense: MIT
+  */
+
+  // make sure direct access to the plugin is blocked
+  if (!defined('ABSPATH')) {
+    die;
+  }
+
+  // define constants
+  define('KDG_FABLAB_IS_VERSION', '1.0.0');
+  define('KDG_FABLAB_IS_PLUGIN_DIR', plugin_dir_path(__FILE__));
+  define('KDG_FABLAB_IS_PLUGIN_PREFIX', 'kdg_fablab_is_');
+
+  // define hooks
+  register_activation_hook(__FILE__, 'plugin_activation'); // execute on activation
+  register_deactivation_hook(__FILE__, 'plugin_deactivation'); // execute on deactivation
+
+  // functions
+  /**
+   * Actions to perform on plugin activation
+   */
+  function plugin_activation() {
+    // code to be executed when plugin is activated
+    set_transient('kdg-fablab-is-admin-notice', true, 5);
+  }
+
+  /**
+   * Actions to perform on plugin deactivation
+   */
+  function plugin_deactivation() {
+    // code to be executed when plugin is deactivated
+  }
