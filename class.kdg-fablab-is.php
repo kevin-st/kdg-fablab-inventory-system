7005f5777eeedaac01afb58daffe1287b82d5c1e
kevin-st <kevin.stollaerd@gmail.com>
2019-04-10 21:09:34 +0200

    Added custom post type: workshop

--- class.kdg-fablab-is.php
+++ class.kdg-fablab-is.php
@@ -44,6 +44,7 @@
      * Enable custom post types for this plugin.
      */
     private static function register_custom_post_types() {
+      // machine post type
       register_post_type('machine',
         [
           'description' => 'Een toestel wordt gebruikt in het KdG Fablab',
@@ -61,6 +62,29 @@
           'query_var' => true,
           'supports' => [ 'title', 'editor', 'excerpt', 'custom-fields' ],
           'rewrite' => ['slug' => 'toestellen'],
+          'taxonomies' => [
+            'category'
+          ]
+        ]);
+
+      // workshop post type
+      register_post_type('workshop',
+        [
+          'description' => 'Een toestel wordt gebruikt in het KdG Fablab',
+          'labels' => [
+            'add_new' => 'Nieuwe workshop',
+            'add_new_item' => 'Nieuwe workshop toevoegen',
+            'all_items' => 'Alle workshops',
+            'edit_item' => 'Workshop bijwerken',
+            'name' => 'Workshops',
+            'singular_name' => 'Workshop'
+          ],
+          'has_archive' => true,
+          'menu_icon' => 'dashicons-calendar',
+          'public' => true,
+          'query_var' => true,
+          'supports' => [ 'title', 'editor', 'excerpt', 'custom-fields' ],
+          'rewrite' => ['slug' => 'workshops'],
           'taxonomies' => [
             'category'
           ]
