784c592e4b10b8e29ac75d095b415abf98b54546
kevin-st <kevin.stollaerd@gmail.com>
2019-04-16 16:44:12 +0200

    Removed: custom fields Added: custom taxonomy, thumbnail support

--- class.kdg-fablab-is-admin.php
+++ class.kdg-fablab-is-admin.php
@@ -12,8 +12,6 @@
       self::$initiated = true;
 
       add_action('admin_notices', array('KdGFablab_IS_Admin', 'kdg_fablab_is_admin_notice'));
-      add_action('add_meta_boxes', array('KdGFablab_IS_Admin', 'kdg_fablab_is_custom_fields_to_edit'));
-      //add_action('save_post', array('KdGFablab_IS_Admin', 'kdg_fablab_is_save_custom_fields_data'), 10, 2 );
     }
 
     /**
@@ -34,35 +32,5 @@
           // delete the transient, so it only gets displayed once
           delete_transient('kdg-fablab-is-admin-notice');
       }
-    }
-
-    public static function kdg_fablab_is_custom_fields_to_edit() {
-      $prfx = "prfx_kdg_fablab_";
-
-      if (defined('KDG_FABLAB_IS_PLUGIN_PREFIX')) {
-        $prfx = KDG_FABLAB_IS_PLUGIN_PREFIX;
-      }
-
-      add_meta_box(
-        ($prfx . 'workshop'),
-        "Opkomende workshop(s)",
-        array('KdGFablab_IS_Admin', 'kdg_fablab_is_show_custom_fields'),
-        'machine',
-        'side'
-      );
-    }
-
-    public static function kdg_fablab_is_show_custom_fields($post) {
-      wp_nonce_field(basename(__FILE__), 'kdg_fablab_is_nonce_field');
-
-      $value = esc_attr(get_post_meta($post->ID, '_' . KDG_FABLAB_IS_PLUGIN_PREFIX . 'value_key', true));
-      ?>
-      <p>
-        <label for="kdg-fablab-workshop"><?php _e("Voeg de naam van een workshop toe waarbij dit apparaat wordt gebruikt.", 'example' ); ?></label>
-      </p>
-      <p>
-        <input class="widefat" type="text" name="kdg-fablab-workshop" id="kdg-fablab-workshop" value="<?php echo $value; ?>" size="30" />
-      </p>
-      <?php
     }
   }
--- class.kdg-fablab-is.php
+++ class.kdg-fablab-is.php
@@ -9,6 +9,7 @@
        if (!self::$initiated) {
          self::init_hooks();
          self::register_custom_post_types();
+         self::register_custom_taxonomies();
        }
      }
 
@@ -18,24 +19,24 @@
     private static function init_hooks() {
       self::$initiated = true;
 
-      add_filter('pre_get_posts', array('KdGFablab_IS', 'query_post_type'));
+      add_filter("pre_get_posts", array("KdGFablab_IS", "query_post_type"));
     }
 
     public static function query_post_type($query) {
       if (is_category()) {
-        $post_type = get_query_var('post_type');
+        $post_type = get_query_var("post_type");
 
         if ($post_type) {
           $post_type = $post_type;
         } else {
           $post_type = [
-            'nav_menu_item',
-            'post',
-            'toestellen'
+            "nav_menu_item",
+            "post",
+            "toestellen"
           ];
         }
 
-        $query->set('post_type', $post_type);
+        $query->set("post_type", $post_type);
         return $query;
       }
     }
@@ -45,49 +46,75 @@
      */
     private static function register_custom_post_types() {
       // machine post type
-      register_post_type('machine',
+      register_post_type("machine",
         [
-          'description' => 'Een toestel wordt gebruikt in het KdG Fablab',
-          'labels' => [
-            'add_new' => 'Nieuw toestel',
-            'add_new_item' => 'Nieuw toestel toevoegen',
-            'all_items' => 'Alle toestellen',
-            'edit_item' => 'Toestel bijwerken',
-            'name' => 'Toestellen',
-            'singular_name' => 'Toestel'
+          "description" => "Een toestel wordt gebruikt in het KdG Fablab",
+          "labels" => [
+            "add_new"       => __("Nieuw toestel"),
+            "add_new_item"  => __("Nieuw toestel toevoegen"),
+            "all_items"     => __("Alle toestellen"),
+            "edit_item"     => __("Toestel bijwerken"),
+            "name"          => __("Toestellen"),
+            "search_items"      => __("Toestel zoeken"),
+            "singular_name" => __("Toestel")
           ],
-          'has_archive' => true,
-          'menu_icon' => 'dashicons-admin-generic',
-          'public' => true,
-          'query_var' => true,
-          'supports' => [ 'title', 'editor', 'excerpt', 'custom-fields' ],
-          'rewrite' => ['slug' => 'toestellen'],
-          'taxonomies' => [
-            'category'
-          ]
+          "has_archive" => true,
+          "menu_icon" => "dashicons-admin-generic",
+          "public" => true,
+          "query_var" => true,
+          "supports" => [ "title", "editor", "excerpt", "thumbnail" ],
+          "rewrite" => ["slug" => "toestellen"]
         ]);
 
       // workshop post type
-      register_post_type('workshop',
+      register_post_type("workshop",
         [
-          'description' => 'Een toestel wordt gebruikt in het KdG Fablab',
-          'labels' => [
-            'add_new' => 'Nieuwe workshop',
-            'add_new_item' => 'Nieuwe workshop toevoegen',
-            'all_items' => 'Alle workshops',
-            'edit_item' => 'Workshop bijwerken',
-            'name' => 'Workshops',
-            'singular_name' => 'Workshop'
+          "description" => "Een toestel wordt gebruikt in het KdG Fablab",
+          "labels" => [
+            "add_new"       => __("Nieuwe workshop"),
+            "add_new_item"  => __("Nieuwe workshop toevoegen"),
+            "all_items"     => __("Alle workshops"),
+            "edit_item"     => __("Workshop bijwerken"),
+            "name"          => __("Workshops"),
+            "search_items"      => __("Workshop zoeken"),
+            "singular_name" => __("Workshop")
           ],
-          'has_archive' => true,
-          'menu_icon' => 'dashicons-calendar',
-          'public' => true,
-          'query_var' => true,
-          'supports' => [ 'title', 'editor', 'excerpt', 'custom-fields' ],
-          'rewrite' => ['slug' => 'workshops'],
-          'taxonomies' => [
-            'category'
-          ]
+          "has_archive" => true,
+          "menu_icon" => "dashicons-calendar",
+          "public" => true,
+          "query_var" => true,
+          "supports" => [ "title", "editor", "excerpt", "thumbnail" ],
+          "rewrite" => ["slug" => "workshops"]
         ]);
+    }
+
+    /**
+     * Enable custom taxonomies for this plugin.
+     */
+    private static function register_custom_taxonomies() {
+      // machine/workshop taxonomy
+      register_taxonomy("kdg_fablab_is_type", [ "machine", "workshop" ], [
+        "hierarchical" => true,
+        "labels" => [
+          "name"              => __("Type"),
+          "singular_name"     => __("Type"),
+          "search_items"      => __("Doorzoek types"),
+          "all_items"         => __("Alle types"),
+          "parent_item"       => __("Hoofdtype"),
+          "parent_item_colon" => __("Hoofdtype"),
+          "edit_item"         => __("Type bijwerken"),
+          "update_item"       => __("Type updaten"),
+          "add_new_item"      => __("Voeg nieuw type toe"),
+          "new_item_name"     => __("Nieuw type"),
+          "menu_name"         => __("Types"),
+          "not_found"         => __("Geen types gevonden.")
+        ],
+        "public" => true,
+        "rewrite" => [ "slug" => "types" ],
+        "show_admin_column" => true
+      ]);
+
+      register_taxonomy_for_object_type("kdg_fablab_is_type", "machine");
+      register_taxonomy_for_object_type("kdg_fablab_is_type", "workshop");
     }
   }
